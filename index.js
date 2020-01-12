1391106407762f570f351e3516f7a46556714ef3
Asheer Rizvi <asheer.vil@gmail.com>
2020-01-12 10:26:15 +0530

    Added skeletal code to the application.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const { log } = require('./startup/log');
+
+require('./startup/errorLogging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+// require('./startup/config')();
+require('./startup/validation')();
+require('./startup/prod')(app);
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    log.info(`Listening on port ${port} :)`);
+});
+module.exports = server;
---
+++ startup/log.js
@@ -0,0 +1,5 @@
+const pino = require('pino');
+
+const log = pino({ prettyPrint: { colorize: true } });
+
+module.exports.log = log;
