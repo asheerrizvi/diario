f528de83edcda81e5e90317190a0f6e27849618f
asheerrizvi <asheer.vil@gmail.com>
2020-02-17 01:08:34 +0530

    Modified the signin request to return a JSON object instead of a plain string.

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.post('/', validate(validateAuth), async (req, res) => {
     if (!validPassword) return res.status(400).send('Invalid email or password.');
 
     const token = user.generateAuthToken();
-    res.send(token);
+    res.send(JSON.stringify(token));
 });
 
 function validateAuth(req) {
